f0aaa6ce9933d7a91106607f562e7979748aba06
lijiatu3427 <lijiatu3427@gmail.com>
2026-06-15 05:55:43 +0800

    完成day16的flask

---
+++ day16/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>首页</h1><p>这是我的第一个 Flask 网站。</p>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>关于我</h1><p>我是电子信息专业研究生，正在学习 Python、AI 和多模态。</p>"
+
+
+@app.route("/project")
+def project():
+    return "<h1>我的项目</h1><p>学生成绩管理系统 V2.1</p>"
+
+@app.route("/contact")
+def contact():
+    return "<h1>联系方式：GitHub: lijiatu3427-maker</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
