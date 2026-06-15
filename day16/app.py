823005bb9fc0744ad98bdd088a15a4bef9319145
lijiatu3427 <lijiatu3427@gmail.com>
2026-06-15 13:41:14 +0800

    添加flask页面模版

--- day16/app.py
+++ day16/app.py
@@ -1,26 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h1>首页</h1><p>这是我的第一个 Flask 网站。</p>"
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return "<h1>关于我</h1><p>我是电子信息专业研究生，正在学习 Python、AI 和多模态。</p>"
+    return render_template("about.html")
 
 
 @app.route("/project")
 def project():
-    return "<h1>我的项目</h1><p>学生成绩管理系统 V2.1</p>"
+    return render_template("project.html")
 
 
 @app.route("/contact")
 def contact():
-    return "<h1>联系方式</h1><p>GitHub: lijiatu3427-maker</p>"
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
